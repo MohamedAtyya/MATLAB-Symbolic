eb9746ae7480f50a943ec04fa9202f47d19e869c
mohamed atyya <50559901+MohamedAtyya@users.noreply.github.com>
2019-06-01 18:23:15 +0200

    Update Symbolic_Example.m

--- Symbolic_Example.m
+++ Symbolic_Example.m
@@ -1,16 +1,18 @@
 %% clear memory 
 clear all; close all; clc;
 %% define symbols
-
+syms x y
 %% symbolic equ
-
+z = x*(x+1)+3*x^2;
 %% expand
-
+z = expand(z);
 %% simplify
-
+simplify(z);
 %% pretty
-
+pretty(z);
 %% symbolic func
-
+f1(x)=x^2;
+f2(x,y) = x+ y
 %% manipulate in func 
-
+f1(3)
+f2(1,5)
